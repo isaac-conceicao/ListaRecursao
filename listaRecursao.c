edf4b5ccb7a54e2f4a0e18bbb821a002b3ac69e0
Isaac Conceição <isaac.academico@gmail.com>
2020-10-25 23:14:04 +0000

    final

--- listaRecursao.c
+++ listaRecursao.c
@@ -21,8 +21,8 @@ int fatorial(int n){
 int fibonacci(int n){
   if(n==1){
     return 0;
-  }else if(n==2){
-    return 1;  
+  }else if(n==2||n==3){
+    return 1; 
   }else{
     return fibonacci(n-1)+fibonacci(n-2);
   }
